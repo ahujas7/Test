8b04632af9aa7fa7015834fd06cd99d7dbc34048
Saksham <63823596+ahujas7@users.noreply.github.com>
2020-12-26 19:58:42 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,7 @@ public class Main
 
   public static void main(String[] args) 
   {
-  	
-	Random gen = new Random();
-	
-	int randomNum = gen.nextInt(100);
-
-	System.out.println(randomNum);
+  
   }
 
 
